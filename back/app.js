c54602ec5deb1a971379d34acc35379200bcc2ee
Alexandre Beauverger <alexandrebeauverger@gmail.com>
2017-09-25 16:45:13 +0200

    ajout de git ignore

--- back/app.js
+++ back/app.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const morgan = require("morgan");
-const bodyParser = require("body-parser");
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const app =express();
 
@@ -11,5 +11,5 @@ const app =express();
 //Start the server
 
 const port = process.env.PORT || 3000;
-app.listen(port)
-console.log("Le serveur marche, t'entend!")
+app.listen(port);
+console.log('Le serveur marche, t\'entend!');
