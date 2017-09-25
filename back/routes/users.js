735174c645b90a7b31fd8cd65130e7441750a534
Alexandre Beauverger <alexandrebeauverger@gmail.com>
2017-09-25 16:48:04 +0200

    ajout de git ignore

--- back/routes/users.js
+++ back/routes/users.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router;
 
-const UsersController = require('../controllers/users')
+const UsersController = require('../controllers/users');
+
 
 router.route('/signup')
 	.post()
