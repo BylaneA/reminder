a958e14058302a72b59cef397a06e39ad02dcddf
Alexandre Beauverger <alexandrebeauverger@gmail.com>
2017-09-25 14:29:53 +0200

    création du serveur

---
+++ back/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+
+const app =express();
+
+//Middlewares
+
+//Routes
+
+//Start the server
+
+const port = process.env.PORT || 3000;
+app.listen(port)
+console.log("Le serveur marche, t'entend!")
