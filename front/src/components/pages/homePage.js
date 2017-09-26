44b1aef0738ea0fad0d472e042a132d040b4b835
Caro83 <nora3110@gmail.com>
2017-09-26 15:35:31 +0200

    Modification de Homepage

--- front/src/components/pages/homePage.js
+++ front/src/components/pages/homePage.js
@@ -3,15 +3,33 @@ import React, { Component } from 'react';
 class Homepage extends Component {
   render() {
     return (
+      <div className="Container-Home">
+      	<div className="triangles">
+	      <div className="redTriangle"></div>
+	     </div>
+      	<div className="Concept">
+      		<h1>Notre concept</h1>
+      		<p>Vide, quantum, inquam, fallare, Torquate. oratio me istius philosophi non 
+offendit; nam et complectitur verbis, quod vult, et dicit plane, quod 
+intellegam; et tamen ego a philosopho, si afferat eloquentiam, non 
+asperner, si non habeat, non admodum flagitem. re mihi non aeque 
+satisfacit, et quidem locis pluribus. sed quot homines, tot sententiae.
+<br />
+Abusus enim multitudine hominum, quam tranquillis in rebus diutius 
+rexit, ex agrestibus habitaculis urbes construxit multis opibus firmas 
+et viribus, quarum ad praesens pleraeque licet Graecis nominibus 
+appellentur, quae isdem ad arbitrium inposita sunt conditoris, 
+primigenia tamen nomina non amittunt, quae eis Assyria lingua 
+institutores veteres indiderunt.
 
-      <div className="Container-fluid">
+</p>
+      		
+      	</div>
 
-      	<div>Home Page</div>
-    
+      
       </div>
-              
+        
+      
     );
   }
 }
-
-export default Homepage;
