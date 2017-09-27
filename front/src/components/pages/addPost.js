5898b101b49226eb29364f98b9f104d4d79a90e7
Carole <leroux.carole@gmail.com>
2017-09-27 17:26:35 +0200

    Modification design AddPost

--- front/src/components/pages/addPost.js
+++ front/src/components/pages/addPost.js
@@ -19,17 +19,25 @@ class AddPost extends Component {
       <div className="labels-addPost">
           <label for="post-title">Titre :</label>
           <label for="post-message">Message :</label>
+          <label for="post-importance">Importance :</label>
         </div>
 
         <div className="inputs-addPost">
           <input type="text" name="post-title" id="post-title"/>
           <textarea type="text" name="post-message" id="post-message" rows="10" cols="25" placeholder="Tapez votre message ici">
           </textarea>
+          <div className="triangles-importance">
+            <div className="green-importance"></div>
+            <div className="orange-importance"></div>
+            <div className="red-importance"></div>
+          </div>
         </div>
         
     </div>
-    <button>ANNULER</button>
-    <button>CREER</button>
+    <div className="buttons-post">
+      <button className="btn-delete-post">ANNULER</button>
+      <button className="btn-create-post">CREER</button>
+    </div>
   </div>
 
 		<div className="triangles">
