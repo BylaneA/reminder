a8ab968379ff11f474b117c90e6dc9837dacbf37
Carole <leroux.carole@gmail.com>
2017-09-27 14:32:54 +0200

    Correction erreur Design

--- front/src/components/pages/connexion.js
+++ front/src/components/pages/connexion.js
@@ -1,38 +1,39 @@
 import React, { Component } from 'react';
 
-import { BrowserRouter, Switch, Route, Link} from 'react-router-dom';
 
 class Connexion extends Component {
   render() {
     return (
-        <div className="container-connexion">
 
-          <div className="triangles">
-            <div className="redTriangle"></div>
-          </div>
+      <div className="container-connexion">
 
-          <div className="concept">
-            <h1>Connexion</h1>
+      	<div className="triangles">
+	      <div className="redTriangle"></div>
+	     </div>
+      	<div className="concept">
+			<h1>Connexion</h1>
 
-            <div className="form-register">
-              <div className="labels-login">
-                <label for="userName">Nom d'utilisateur 
-                <br />ou email:</label>
-                <label for="password">Mot de passe :</label>
-              </div>
+			<div className="form-register">
+				<div className="labels-login">
+				  <label for="userName">Nom d'utilisateur 
+				  <br />ou email:</label>
+				  <label for="password">Mot de passe :</label>
+				</div>
 
-              <div className="inputs-login">
-                <input type="text" name="userName" id="userName"/>
-                <input type="text" name="password" id="password"/>
-              </div>
-            </div>
+				<div className="inputs-login">
+				  <input type="text" name="userName" id="userName"/>
+				  <input type="text" name="password" id="password"/>
+				</div>
+			</div>
 
-            <div className="link-forgot"><a href="#">Mot de passe oublié</a></div>
-            
-              <button className="button-login">Se connecter</button>
+			<div className="link-forgot"><a href="#">Mot de passe oublié</a></div>
+			<button className="button-login">Se connecter</button>
+          
 
-          </div>  
-        </div> 
+      	</div>
+
+      </div>
+        
       
     );
   }
